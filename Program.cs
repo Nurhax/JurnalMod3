0ac6651790d51ec5089d0f049d0a52b0530df434
Nurhax <iqbalnur2009@gmail.com>
2024-03-13 12:56:43 +0700

    menambahkan class KodeBuah

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class KodeBuah
+{
+    public enum namaBuah
+    {
+        Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry,
+        Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+    }
+
+    public static string getKodeBuah(namaBuah buah)
+    {
+        string[] KodeBuah = {"A00","B00","C00","D00","E00","F00"
+                ,"H00","I00","J00","K00","L00","M00","N00","O00"};
+        return KodeBuah[(int)buah];
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        KodeBuah.namaBuah namaBuah = KodeBuah.namaBuah.Semangka;
+        string Buah = KodeBuah.getKodeBuah(namaBuah);
+        Console.WriteLine("Nama Buah:" + namaBuah);
+        Console.WriteLine("Kode Buah:" + Buah);
+    }
+}
